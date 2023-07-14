26859f47e861493e0d5f3dd3291847bebdd524c2
ksenafiy <ksuxilda@yandex.ru>
2023-07-14 15:50:00 +0300

    Add credits

--- src/js/consts.js
+++ src/js/consts.js
@@ -1,3 +1,5 @@
-export const WORDS = ["code", "frontend", "programming"];
+const wordList =
+  "declaration variables source output input programming languages constants types procedures functions random generator array matrix index files console libraries debugging modules classes form algorithm compilation elements counter";
+export const WORDS = wordList.split(" ");
 
 export const KEYWORD_LETTERS = Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
--- src/js/game.js
+++ src/js/game.js
@@ -43,6 +43,16 @@ const createHangmanImg = () => {
   return image;
 };
 
+const createDeveloperPhoto = () => {
+  const image = document.createElement("img");
+  image.src = "images/developer-photo.JPG";
+  image.alt = "developer photo";
+  image.classList.add("developer-photo");
+  image.id = "developer-photo";
+
+  return image;
+};
+
 const checkLetter = (letter) => {
   const word = sessionStorage.getItem("word");
   const inputLetter = letter.toLowerCase();
@@ -73,6 +83,20 @@ const checkLetter = (letter) => {
   }
 };
 
+const credits = () => {
+  const developerPhoto = createDeveloperPhoto();
+  logoH1.classList.add("logo-sm");
+
+  gameDiv.innerHTML = '<h1 class="uppercase mt-2">Nikitina Ksenia</h1>';
+  gameDiv.innerHTML += '<p class="font-medium">Frontend developer</p>';
+  gameDiv.innerHTML += "<p>https://github.com/ksenafiy</p>";
+  gameDiv.innerHTML +=
+    '<button id="return-to-start" class="button-primary button-return mt-6">Return to start</button>';
+
+  gameDiv.prepend(developerPhoto);
+  document.getElementById("return-to-start").onclick = startScreen;
+};
+
 const stopGame = (status) => {
   document.getElementById("placeholders").remove();
   document.getElementById("tries").remove();
@@ -97,6 +121,18 @@ const stopGame = (status) => {
     "game"
   ).innerHTML += `<p>The word was: <span class="result-word">${word}</span></p><button id="play-again" class="button-primary px-5 py-2 mt-5">Play again</button>`;
   document.getElementById("play-again").onclick = startGame;
+  document.getElementById(
+    "game"
+  ).innerHTML += `<button id="developer" class="button-developer px-5 py-2 mt-5">Click to see the developer :)</button>`;
+  document.getElementById("developer").onclick = credits;
+  document.getElementById("play-again").onclick = startGame;
+};
+
+const startScreen = () => {
+  logoH1.classList.remove("logo-sm");
+  gameDiv.innerHTML =
+    '<button id="startGame" class="button-primary">Start game</button>';
+  document.getElementById("startGame").addEventListener("click", startGame);
 };
 
 export const startGame = () => {
